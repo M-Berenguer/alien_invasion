039b055ab31e14f57ccf9d167d10ebac172b7c88
Marcos Berenguer <135846391+MarcosBerenguer-ALTEN@users.noreply.github.com>
2025-03-05 17:10:37 +0100

    Adjust ship speed

--- settings.py
+++ settings.py
@@ -14,3 +14,6 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         self.caption = "Alien Invasion"
+
+        # Ship settings
+        self.ship_speed = 1.5
