ebe8f570922f13825862fce4336c04e23b613eb4
Marcos Berenguer <135846391+MarcosBerenguer-ALTEN@users.noreply.github.com>
2025-03-06 09:50:38 +0100

    Build alien fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -93,8 +93,28 @@ class AlienInvasion:
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
+        # Create an alien and find the number of aliens in a row.
+        # Spacing between each alien is equal to one alien width and other alien height
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width, alien_height = alien.rect.size
+
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+            
+            # Move to the next row
+            current_x = alien_width
+            current_y += 2 * alien_height
+
+    def _create_alien(self, x_position, y_position):
+        """Create an alien and place it in the row."""
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
+        self.aliens.add(new_alien)
 
     def _update_screen(self):
         """Update images on the screen and flip to the new screen."""
